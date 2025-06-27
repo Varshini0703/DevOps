f8f19094e27fd89819ed41ff53c4335024701c5d
Varshini0703 <23wh1a05h2@bvrithyderabad.edu.in>
2025-06-27 16:53:36 +0530

    Initial commit

---
+++ EventRegistration/server/models/user.js
@@ -0,0 +1,16 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const dbConfig = require('../config/db.config');
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect
+});
+
+const User = sequelize.define('user', {
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  event: DataTypes.STRING
+});
+
+sequelize.sync(); // Creates table if not exim.
+module.exports = { User, sequelize };
